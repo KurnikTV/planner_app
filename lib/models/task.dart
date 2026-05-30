4f3820e37ac751d33b343e0bafa1f51b7039123b
KurnikTV <csmensuperlego@gmail.com>
2026-05-30 15:03:52 +0300

    Add project structure and Task model

---
+++ lib/models/task.dart
@@ -0,0 +1,13 @@
+class Task {
+  String title;
+  String description;
+  DateTime startTime;
+  DateTime endTime;
+
+  Task({
+    required this.title,
+    required this.description,
+    required this.startTime,
+    required this.endTime,
+  });
+}
